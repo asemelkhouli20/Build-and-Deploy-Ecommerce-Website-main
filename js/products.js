95961f68c73e568cba1cdd31e0c152e969ec804e
asemelkhouli20 <asemelkhouli209@gmail.com>
2024-05-25 13:07:52 +0300

    new

---
+++ js/products.js
@@ -0,0 +1,119 @@
+export const newProduct = [
+  {
+    image: "n1",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$87",
+    rating: 5,
+  },
+  {
+    image: "n2",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$87",
+    rating: 5,
+  },
+  {
+    image: "n3",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$87",
+    rating: 5,
+  },
+  {
+    image: "n4",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$87",
+    rating: 5,
+  },
+  {
+    image: "n5",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$87",
+    rating: 5,
+  },
+  {
+    image: "n6",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$87",
+    rating: 5,
+  },
+  {
+    image: "n7",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$87",
+    rating: 5,
+  },
+  {
+    image: "n8",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$87",
+    rating: 5,
+  },
+];
+
+
+
+export const featureProduct = [
+  {
+    image: "f1",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$545",
+    rating: 4,
+  },
+  {
+    image: "f2",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$97",
+    rating: 2,
+  },
+  {
+    image: "f3",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$77",
+    rating: 4,
+  },
+  {
+    image: "f4",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$47",
+    rating: 5,
+  },
+  {
+    image: "f5",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$37",
+    rating: 4,
+  },
+  {
+    image: "f6",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$27",
+    rating: 2,
+  },
+  {
+    image: "f7",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$78",
+    rating: 4,
+  },
+  {
+    image: "f8",
+    brand: "adidas",
+    title: "Cartoon Astronaut T-shirts",
+    price: "$57",
+    rating: 3,
+  },
+];
---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Script for navigation bar
+import { newProduct, featureProduct } from "./products.js";
+
+setProduct("products", featureProduct);
+setProduct("NewProducts",newProduct);
+
+
+function setProduct(id,array) {
+  var htmlCode = "";
+  var products = document.getElementById(id);
+  for (let i = 0; i < array.length; i++) {
+    var item = array[i];
+    var ratingHtml = "";
+    for (let j = 0; j < item.rating; j++) {
+      ratingHtml += '<i class="fas fa-star"></i>';
+    }
+    var item = `
+      <div class="pro">
+        <img src="/img/products/${item.image}.jpg" />
+        <div class="des">
+          <span>${item.brand}</span>
+          <h5>${item.title}</h5>
+          <div class="star">${ratingHtml}</div>
+          <h4>${item.price}</h4>
+        </div>
+        <a href="#">
+          <i class="fal fa-shopping-cart cart"></i>
+        </a>
+      </div>
+    `;
+    htmlCode += item;
+  }
+  products.innerHTML = htmlCode;
+}
